a074764c24b95a619539fcb7f7c692e5431d475b
Robin <robinja@stud.ntnu.no>
2026-03-16 22:20:43 +0100

    Added defaulthandler For now there is no way to handle requests that are'nt valid.
    The default handler handles requests that are not
    to any of the specified endpoints and provides the
    user with a message that the page is not found
    along with the "404 not found" header. The reason for
    the making of the defaulthandler now is that a handler
    to test the "SetMessageForLogger" function was needed.
    
    The benefit of this, is that the user is told that the requested
    resource does not exist so that the user can try again with
    a valid request.

---
+++ internal/handlers/defaultHandler.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"assignment-2/utils"
+	"net/http"
+)
+
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	_, err := w.Write([]byte("404 page not found"))
+	if err != nil {
+		utils.SetMessageForLogger(w, "Error writing response from default handler")
+	}
+}
