12b859969de61a7ca55ebb48f873c9f13d2ba629
lennart <lennart@ntnu>
2026-03-18 22:55:02 +0100

    authentication(feature):simple integrity check of api key, if not ok, make another one
    Checks api key for errors such as empty or only standard start string: "sk-envdash-"
    
    Always important to validate that key is good before sending
    
    Currently hardcoded number of times for how many times we will check if there are duplicate or error in key. Currently at 10 tries, probably to high.
    Also not yet connected to Firestore, only simple validation for now

--- internal/handlers/authentication.go
+++ internal/handlers/authentication.go
@@ -54,8 +54,24 @@ func RegisterAuth(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//creating api key
-	createAPI, timeCreateApi := createAPIKey(login.Email)
+	// Generate API key for the user
+	var createAPI, timeCreateApi string
+	//maxAttempt should be a const, but I want to ask team member or TA for what is a reasonable number
+	//max attempt is used to avoid loop with checking api key duplicate!
+	maxTestAttempts := 10
+	for i := 0; i < maxTestAttempts; i++ {
+		createAPI, timeCreateApi = createAPIKey(login.Email)
+		if !isAPIKeyUsed(createAPI) {
+			break
+		}
+		//this would only be in the senario where some number breakes the md5 hash or full storage in database
+		//then it would be an infinte loop of generating api keys, this should not happen, so we want to break this after a certain number of attempts and log it
+		if i == maxTestAttempts-1 {
+			fmt.Printf("Failed to generate a unique API key after %d attempts\n", i+1)
+			http.Error(w, "Failed to generate a unique API key after several attempts", http.StatusLoopDetected)
+			return
+		}
+	}
 
 	//formatting the response to the user, which includes the generated API key and the time of API creation
 	key := key{
@@ -68,7 +84,7 @@ func RegisterAuth(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("API key generated and sent to user:", key.Key)
+	fmt.Println("API key generated and sent to user")
 
 }
 
@@ -90,4 +106,31 @@ func createAPIKey(email string) (string, string) {
 	fmt.Println("createAPI:", createAPI)
 
 	return createAPI, timeCreateApi
+}
+
+func isAPIKeyUsed(key string) bool {
+	//check if api key is already in use, if so, generate a new one (this is very unlikely, but we want to be sure)
+	usedAPIKey := "sk-envdash-4597dc2d89e56c8e0cde3d3b9f42bdfa" // simulates a used api key
+
+	//here we want to check if the generated API key were made or not, if no api key were made, send true
+	if key == "" {
+		//Logging not implemented, but needs to be logged because this should not happen
+		//log.Println("Generated API key is empty")
+		return true
+	}
+	//if only the start of the API key is generated, send true, because this means that the random string part is not generated yet, and this is not a valid API key
+	if key == "sk-envdash-" {
+		//logging waiting for implementation
+		//log.Println("Generated API key is incomplete")
+		return true
+	}
+
+	//when firebase is added this will be where api key is looking for duplicates
+	if key == usedAPIKey {
+		//logging waiting for implementation
+		//log.Println("Generated API key is already in use")
+		return true
+	}
+	return false
+
 }
